a4fad34f8ac51d62d98c222bc0a0baf65a1c7b73
= <=>
2019-12-04 09:16:10 +0800

    修改resnet加载模型的方式

--- models/rgb_resnet.py
+++ models/rgb_resnet.py
@@ -251,7 +251,17 @@ def rgb_resnet101(pretrained=False, **kwargs):
     """
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet101'],model_dir='/home/xt/two-stream-pytorch/models'))
+        # model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+        pretrained_dict = model_zoo.load_url(url = model_urls['resnet152'],model_dir='/home/xt/two-stream-pytorch/models')
+        model_dict = model.state_dict()
+
+        # 1. filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        # 2. overwrite entries in the existing state dict
+        model_dict.update(pretrained_dict)
+        # 3. load the new state dict
+        model.load_state_dict(model_dict)
+
     return model
 
 
