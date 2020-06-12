91943066a0736bca7ea9059d5444a8336d915871
Tao <2761564455@qq.com>
2020-06-12 17:07:05 +0800

    修改pre1[0]为pre1.item()

--- main_single_gpu.py
+++ main_single_gpu.py
@@ -232,7 +232,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # measure accuracy and record loss
         prec1, prec3 = accuracy(output.data, target, topk=(1, 3))
-        acc_mini_batch += prec1[0].item()
+        acc_mini_batch += prec1.item()
         loss = criterion(output, target_var)
         loss = loss / args.iter_size
         loss_mini_batch += loss.itme()
